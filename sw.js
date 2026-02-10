166f8411cb3880e711f23685b5c74ee482c9023d
Sergio <k9w9wqjzkb@privaterelay.appleid.com>
2026-02-10 23:23:29 +0100

    versione 2

--- sw.js
+++ sw.js
@@ -8,13 +8,36 @@ const ASSETS = [
   './style.css',
   './app.js',
   './dashboard.js',
-  './manifest.json'
+  './manifest.json',
+  'https://cdn.jsdelivr.net/npm/chart.js' // Salviamo anche la libreria dei grafici
 ];
 
+// Fase di installazione: scarica i file e li salva nella memoria del telefono
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log('Cache creata: file salvati per l\'offline');
+      return cache.addAll(ASSETS);
+    })
+  );
 });
 
+// Fase di attivazione: pulisce vecchie versioni della cache
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(keys.map(key => {
+        if (key !== CACHE_NAME) return caches.delete(key);
+      }));
+    })
+  );
+});
+
+// Gestione delle richieste: se non c'è internet, usa i file in cache
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
 });
